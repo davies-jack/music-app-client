3b5c95bee51f3f4cbf064962f60f8e20bd5f45a4
davies-jack <jackethdavies@hotmail.com>
2021-07-11 14:31:56 +0100

    adding search input for the Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,33 @@
-import React, { ReactElement } from "react";
+import { useState, ReactElement } from "react";
 
 type Props = {
   code: string;
 };
 
 export default function Dashboard({ code }: Props): ReactElement {
-  return <div>Dashboard</div>;
+  const [search, setSearch] = useState<string>("");
+
+  const handleSearchChange = (e: { target: { value: string } }) => {
+    const { value } = e.target;
+    if (value !== search) setSearch(value);
+  };
+
+  return (
+    <div>
+      <h2>Welcome to the Dashboard</h2>
+      <p>
+        You're able to search for artists, or song names in the input below.
+      </p>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="search-field">Song name, or artist</label>
+        <input
+          type="text"
+          name="search-field"
+          id="search-field"
+          onChange={handleSearchChange}
+        />
+      </form>
+    </div>
+  );
 }
