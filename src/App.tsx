e204ec28bba43076cbcba69c77958b3a1a4e452f
davies-jack <jackethdavies@hotmail.com>
2021-07-11 14:26:00 +0100

    adding Dashboard & Home components depending on login code in the url

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+
+const getLoginCode = new URLSearchParams(window.location.search).get("code");
 
 function App() {
   return (
     <div>
       <h1>music app</h1>
+
+      {getLoginCode ? <Dashboard code={getLoginCode.toString()} /> : <Home />}
     </div>
   );
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+type Props = {
+  code: string;
+};
+
+export default function Dashboard({ code }: Props): ReactElement {
+  return <div>Dashboard</div>;
+}
