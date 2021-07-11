844ba2244cdbf7f76ecabdebd4ccd5c96f8083cf
davies-jack <jackethdavies@hotmail.com>
2021-07-11 15:27:03 +0100

    making useAuth hook refresh our access token when needed

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function useAuth(code: string): string | null {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [refreshToken, setRefreshToken] = useState<string | null>(null);
-  const [expiresIn, setExpiresIn] = useState<string | null>(null);
+  const [expiresIn, setExpiresIn] = useState<number | null>(null);
 
   useEffect(() => {
     axios
@@ -18,7 +18,24 @@ export default function useAuth(code: string): string | null {
       })
       .catch((err) => (window.location.href = "/"));
   }, [code]);
-  useEffect(() => {}, [refreshToken, expiresIn]);
+  useEffect(() => {
+    if (refreshToken && expiresIn) {
+      // Using setInterval here to only call this api call if the expiresIn value is just about to run out, so we're not doing it when we do not need to.
+      const interval = setInterval(() => {
+        axios
+          .post("http://localhost:8080/auth/refresh", { refreshToken })
+          .then(({ data: { accessToken, expiresIn } }) => {
+            setAccessToken(accessToken);
+            setExpiresIn(expiresIn);
+          })
+          .catch((err) => {
+            window.location.href = "/";
+            console.error(err);
+          });
+      }, (expiresIn - 60) * 1000);
+      return () => clearInterval(interval);
+    }
+  }, [refreshToken, expiresIn]);
 
   return accessToken;
 }
