63bc90e496dbdee9d45c861724341254606041ef
davies-jack <jackethdavies@hotmail.com>
2021-07-11 14:39:58 +0100

    making SearchForm it's own component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,13 +4,33 @@ type Props = {
   code: string;
 };
 
-export default function Dashboard({ code }: Props): ReactElement {
-  const [search, setSearch] = useState<string>("");
-
+function SearchForm({
+  search,
+  setSearch,
+}: {
+  search: string;
+  setSearch: (value: string) => void;
+}): ReactElement {
   const handleSearchChange = (e: { target: { value: string } }) => {
     const { value } = e.target;
     if (value !== search) setSearch(value);
   };
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="search-field">Song name, or artist</label>
+      <input
+        type="text"
+        name="search-field"
+        id="search-field"
+        onChange={handleSearchChange}
+      />
+    </form>
+  );
+}
+
+export default function Dashboard({ code }: Props): ReactElement {
+  const [search, setSearch] = useState<string>("");
 
   return (
     <div>
@@ -18,16 +38,9 @@ export default function Dashboard({ code }: Props): ReactElement {
       <p>
         You're able to search for artists, or song names in the input below.
       </p>
+      <SearchForm search={search} setSearch={setSearch} />
 
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="search-field">Song name, or artist</label>
-        <input
-          type="text"
-          name="search-field"
-          id="search-field"
-          onChange={handleSearchChange}
-        />
-      </form>
+      <div className="searchResults"></div>
     </div>
   );
 }
